85702e8d4e58b3199549bcb140210b9e80546525
Dani Milano <danimilano6@gmail.com>
2023-06-10 22:45:48 -0300

    estilo header vuelve

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -20,24 +20,9 @@ const BoxSx = () => {
         '&:hover': {
         backgroundColor: 'primary.main', opacity: [0.9, 0.8, 0.7],          
         }, }}>
-      <div>
-             <img className="ui medium circular image fotoHeader2" src="https://images.unsplash.com/photo-1617049690922-2014b68ee066?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max" />
-      </div>
-      <row>
-      <div>
-            <img className="ui medium circular image fotoHeader" src="https://images.unsplash.com/photo-1559582125-d397e59103f0?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"/>
-      </div>    
-      <div>
-            <img className="ui medium circular image fotoHeader3" src="https://images.unsplash.com/photo-1584873564166-f69123e2083e?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max" />
-      </div>
-      <div>
-            <img className="ui medium circular image fotoHeader4" src="https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max" />
-      </div>    
-  
-      </row>
-      <div>
-            <h1 className='titulo'>Viajando<AirplaneTicketIcon></AirplaneTicketIcon></h1>
-      </div>
+      <img className="ui medium circular image" src="https://images.unsplash.com/photo-1435783099294-283725c37230?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4NTc1NDg2MA&ixlib=rb-4.0.3&q=80&w=800"/>      
+      <h1 className='titulo'>Viajando<AirplaneTicketIcon></AirplaneTicketIcon></h1>
+      
     </Box>    
     </React.Fragment>
   );

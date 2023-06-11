b83e03bd4474178d4f6217fa3616ab10eb8fa89f
Dani Milano <danimilano6@gmail.com>
2023-06-10 22:40:32 -0300

    creo en json y test con estilos

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -20,9 +20,24 @@ const BoxSx = () => {
         '&:hover': {
         backgroundColor: 'primary.main', opacity: [0.9, 0.8, 0.7],          
         }, }}>
-      <img className="ui medium circular image" src="https://source.unsplash.com/3Z70SDuYs5g/800x300"/>      
-      <h1 className='titulo'>Viajando<AirplaneTicketIcon></AirplaneTicketIcon></h1>
-      
+      <div>
+             <img className="ui medium circular image fotoHeader2" src="https://images.unsplash.com/photo-1617049690922-2014b68ee066?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max" />
+      </div>
+      <row>
+      <div>
+            <img className="ui medium circular image fotoHeader" src="https://images.unsplash.com/photo-1559582125-d397e59103f0?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"/>
+      </div>    
+      <div>
+            <img className="ui medium circular image fotoHeader3" src="https://images.unsplash.com/photo-1584873564166-f69123e2083e?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max" />
+      </div>
+      <div>
+            <img className="ui medium circular image fotoHeader4" src="https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max" />
+      </div>    
+  
+      </row>
+      <div>
+            <h1 className='titulo'>Viajando<AirplaneTicketIcon></AirplaneTicketIcon></h1>
+      </div>
     </Box>    
     </React.Fragment>
   );
